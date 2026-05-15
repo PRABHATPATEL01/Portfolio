2e9e4215485753b3ef75fd4c04055e33ae4c94e8
Prabhat Patel <107596143+PRABHATPATEL01@users.noreply.github.com>
2026-05-15 09:02:12 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+
+if (isGithubActions) {
+  // Extract repository name from GITHUB_REPOSITORY
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+  repo = `/${repoName}`;
+}
+
 const nextConfig: NextConfig = {
   devIndicators: false,
   poweredByHeader: false,
+  output: "export",
+  basePath: repo,
+  assetPrefix: repo,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
